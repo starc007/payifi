4cd86f22ab35a689cd986c7990c87393ff853cb1
Saurabh Chauhan <infinityincorporative51@gmail.com>
2021-04-21 13:28:48 +0530

    updated slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -9,41 +9,40 @@ const Slider = () => {
 	return (
 		<Carousel className="slider">
 			<Carousel.Item slide>
-				<img src={Slider1} alt="First slide" />
+				<img src={Slider1} alt="First slide" className="img-style" />
 				<Carousel.Caption>
 					<div className="flex justify-between">
 						<div className="md:leading-none leading-3 mt-4 md:m-0">
-							<label className="font-bold text-white md:text-3xl text-xs ">
-								<span className="font-extrabold md:text-6xl text-lg text-color-main animate-pulse">
+							<label className="md:font-bold font-medium text-white md:text-3xl text-xs ">
+								<span className="font-extrabold md:text-6xl text-2xl text-color-main animate-pulse">
 									Payifi
 								</span>{" "}
 								is trusted by lakhs
 							</label>
 							<br />
-							<label className="font-bold text-white md:text-3xl text-xs">
+							<label className="md:font-bold font-medium text-white md:text-3xl text-sm">
 								of Indian Retailers
 							</label>
 							<br />
-							<label className="font-bold text-white md:text-3xl text-xs">
+							<label className="md:font-bold font-medium text-white md:text-3xl text-sm">
 								Become a Payifi Sathi
 							</label>
 							<br />
 							{/* <div className=""> */}
-							<button className=" btn-style focus:outline-none md:h-10 h-5 w-32 text-xs md:w-64 md:text-base rounded-xl mb-4 mt-2">
+							<button className=" btn-style focus:outline-none md:h-10 h-5 w-56 text-xs md:w-64 md:text-base rounded-lg mb-4 mt-2">
 								CREATE ACCOUNT FOR DISTRIBUTOR
 							</button>
 							<br />
-							<button className=" btn-style focus:outline-none md:h-10 h-5 w-44 text-xs md:w-64 md:text-base rounded-xl  ">
+							<button className=" btn-style focus:outline-none md:h-10 h-5 w-56 text-xs md:w-64 md:text-base rounded-lg  ">
 								CREATE ACCOUNT FOR RETAILER
 							</button>
 							{/* </div> */}
 						</div>
-						
 					</div>
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item slide>
-				<img src={Slider2} alt="Second slide" />
+				<img src={Slider2} alt="Second slide" className="img-style" />
 
 				<Carousel.Caption>
 					<h3>Second slide label</h3>
@@ -51,7 +50,7 @@ const Slider = () => {
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item slide>
-				<img src={Slider3} alt="Second slide" />
+				<img src={Slider3} alt="Second slide" className="img-style" />
 
 				<Carousel.Caption>
 					<h3>Second slide label</h3>
